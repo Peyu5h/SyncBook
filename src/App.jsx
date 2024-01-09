c5498a519441ba3c86b098b54aae53da485e1ac0
Piyush Bagul <piyushvbagul916@gmail.com>
2024-01-09 14:28:48 +0530

    initial files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Feeling from "./svg/feeling";
+const App = () => {
+  return (
+    <div>
+      <div className="text-xl">Hellllo</div>
+      <div className="all_friends_icon"></div>
+      <Feeling />
+    </div>
+  );
+};
+
+export default App;
