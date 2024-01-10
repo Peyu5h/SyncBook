f9000ab92994a92de7b96ab8c43ccfa725ba7bfc
Piyush Bagul <piyushvbagul916@gmail.com>
2024-01-10 17:48:52 +0530

    auth backend done

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,10 +1,13 @@
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
 const {
   validateEmail,
   validateLength,
   validateUsername,
 } = require("../helpers/validate");
 const bcrypt = require("bcrypt");
+const generateToken = require("../helpers/token");
+const { sendVerificationEmail } = require("../helpers/mailer");
 
 exports.register = async (req, res) => {
   try {
@@ -13,7 +16,6 @@ exports.register = async (req, res) => {
       last_name,
       email,
       password,
-      username,
       bYear,
       bMonth,
       bDay,
@@ -58,7 +60,7 @@ exports.register = async (req, res) => {
       first_name,
       last_name,
       email,
-      password,
+      password: cryptedPassword, // Use the hashed password
       username: newUsername,
       bYear,
       bMonth,
@@ -66,9 +68,76 @@ exports.register = async (req, res) => {
       gender,
     }).save();
 
-    // Return the user information in the response
-    res.json(user);
+    const emailVerificationToken = generateToken(
+      { id: user._id.toString() },
+      "30m"
+    );
+
+    const url = `${process.env.BASE_URL}/activate/${emailVerificationToken}`;
+    sendVerificationEmail(user.email, user.first_name, url);
+
+    const token = generateToken({ id: user._id.toString() }, "7d");
+    return res.json({
+      id: user._id,
+      username: user.username,
+      picture: user.picture,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      token: token,
+      verified: user.verified,
+      message:
+        "Registration successful! Please check your email for verification",
+    });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+exports.activateAccount = async (req, res) => {
+  try {
+    const { token } = req.body;
+    const user = jwt.verify(token, process.env.JWT_SECRET);
+    const check = await User.findById(user.id);
+    if (check.verified == true) {
+      return res.status(400).json({ message: "Account already verified" });
+    } else {
+      await User.findByIdAndUpdate(user.id, { verified: true });
+      return res.status(200).json({ message: "Account verified successfully" });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: "Invalid email address" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res
+        .status(400)
+        .json({ message: "Invalid password, Please try again." });
+    }
+
+    const token = generateToken({ id: user._id.toString() }, "7d");
+    res.send({
+      id: user._id,
+      username: user.username,
+      picture: user.picture,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      token: token,
+      verified: user.verified,
+      message: "Login successful!",
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
   }
 };
